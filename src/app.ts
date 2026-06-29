455cec77e74f463fe4e4be5195aad4ac7631db32
Al-Amin <alamin50cse.miu@gmail.com>
2026-06-29 22:08:41 +0600

    updated login page

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'otp-token'],
   })
 );
 app.use(express.json());
